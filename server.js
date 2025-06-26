ffb6f9443db1e9ec30fcb30dc45059fbca4522b4
Sowmya27904 <sowmyamallipati@gmail.com>
2025-06-26 10:45:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
